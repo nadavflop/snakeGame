36690f61756dafbcd75d2ae2fffbfaf895ad6ed9
nadavflop <nadav9660@gmail.com>
2026-07-01 13:42:18 +0300

    Implement basic Snake game functionality

--- script.js
+++ script.js
@@ -1,9 +1,120 @@
-let userInput= prompt("write a number here");
-function bob(){
-    if ((userInput * userInput) %2 ==0){
-        document.writeln("<div id='f'>"+"yay it works!"+"</div>")
+let grid = document.getElementById('grid');
+let scoreDisplay= document.getElementById('score');
+let squares= [];
+let currentSnake=[2,1,0];
+let direction=1;
+let appleIndex=0;
+let score= 0;
+let timerId=0;
+let intervalTime=200;
+
+//creating the board
+function createBoard(){
+    for (let i=0; i<400; i++){
+        const square = document.createElement('div');
+        grid.appendChild(square);
+        squares.push(square);
     }
-    document.writeln("<div id='ff'>"+Math.sqrt(userInput)+"</div id='ff'>")
+}
+createBoard();
+startGame();
+
+function startGame(){
+    //reset the game
+    currentSnake.forEach(index => squares[index].classList.remove('snake'));
+    squares[appleIndex].classList.remove('apple');
+    clearInterval(timerId);
+    currentSnake=[2,1,0];
+    score=0; direction =1; intervalTime=200;
+    scoreDisplay.textContent=score;
+    currentSnake.forEach(index => squares[index]. classList.add('snake'));
+    generateApple();
+    timerId= setInterval(move,intervalTime)
+
+
 }
 
-bob();
+ function endGame (){
+    return clearInterval(timerId);
+ }    
+function move(){
+    
+    let hitBot=(currentSnake[0]  >=380 && direction === 20);
+    let hitTop= (currentSnake[0] -20 <0 && direction === -20);
+    let hitRight= (currentSnake[0] % 20 ===19 && direction === 1);
+    let hitLeft= (currentSnake[0] % 20 === 0 && direction ===-1);
+    let hitSelf= squares[currentSnake[0] + direction]?.classList.contains('snake')
+    if (hitRight||hitBot||hitTop||hitLeft|| hitSelf) {
+        return clearInterval(timerId);
+    }
+    const tail = currentSnake.pop();
+    const newHead =currentSnake[0] +direction;
+    squares[tail].classList.remove('snake');
+    squares[newHead].classList.add('snake');
+    currentSnake.unshift(newHead);
+    if (squares[newHead].classList.contains('apple')){
+        squares[newHead].classList.remove('apple');
+        squares[tail].classList.add('snake');
+        currentSnake.push(tail);
+        score++; scoreDisplay.textContent= score;
+        generateApple();
+    }
+    
+    document.addEventListener('touchstart', e=> {
+        touchStartx= e.changedTouches[0].screenX;
+        touchStarty= e.changedTouches[0].screenY;
+    }   ,false);
+
+    document.addEventListener('touchend' , e=> {
+        touchEndx = e.changedTouches[0].screenX;
+        touchEndy = e.changedTouches[0].screenY;
+        handleSwipe();
+    },false)
+
+
+} 
+
+ function handleSwipe(){
+        const dx = touchEndx- touchStartx;
+        const dy = touchEndy- touchStarty;
+
+        const absDx= Math.abs(dx);
+        const absDy= Math.abs(dy);
+
+        if (Math.max(absDx, absDy) > 30 ){
+            if (absDx >absDy){
+                if (dx >0) changeDir(-1);
+                else changeDir(1);    
+
+            } else {
+                if (dy > 0) changeDir(20);
+                else changeDir(-20);
+            }
+       }
+    }
+
+
+function generateApple(){
+    do{
+        appleIndex= Math.floor(Math.random()* squares.length);
+       } while (squares[appleIndex]. classList.contains('snake'));
+       squares[appleIndex].classList.add('apple');
+
+}
+
+function changeDir(newDir){
+    //no
+    if (direction+ newDir !== 0) {
+        direction= newDir;
+    }
+}
+
+document.addEventListener('keydown', (E)=> {
+    if(E.key === 'ArrowUp') changeDir(-20);
+    if(E.key=== 'ArrowDown') changeDir(20);
+    if(E.key === 'ArrowLeft') changeDir(-1);
+    if(E.key === 'ArrowRight') changeDir(1);
+
+});
+
+
